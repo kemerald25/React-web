669f4047738419e4bd2aaebc81e1d11af7780383
Dev Royale <unityekeoba18@gmail.com>
2023-12-29 16:14:45 +0100

    The website is done all responsiveness done remaining vital info to be provided by the owner

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import '../styles.css';
-import '../index.css'
+import '../index.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    useEffect(() => {
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-items");
+
+        const handleClick = () => {
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+        };
+
+        hamburger.addEventListener("click", handleClick);
+
+        document.querySelectorAll(".link").forEach((n) =>
+            n.addEventListener("click", () => {
+                hamburger.classList.remove("active");
+                navMenu.classList.remove("active");
+            })
+        );
+
+        // Cleanup the event listeners when the component unmounts
+        return () => {
+            hamburger.removeEventListener("click", handleClick);
+
+            document.querySelectorAll(".link").forEach((n) =>
+                n.removeEventListener("click", () => {
+                    hamburger.classList.remove("active");
+                    navMenu.classList.remove("active");
+                })
+            );
+        };
+    }, []);
+
     return (
         <nav>
             <div className="navbar">
@@ -22,9 +53,15 @@ function Navbar() {
                         <Link to="/contact" className="link">Contact</Link>
                     </li>
                 </ul>
+
+                <div className="hamburger">
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Navbar;
